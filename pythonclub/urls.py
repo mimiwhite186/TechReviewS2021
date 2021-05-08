2a9fcbb79f09058302eadf283025195fab11d093
Minnie Yang <mimiwhite186@gmail.com>
2021-05-07 22:45:02 -0700

    index

--- pythonclub/urls.py
+++ pythonclub/urls.py
@@ -1,22 +1,42 @@
 """pythonclub URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tech/', include('tech.urls')),
-]
+The `urlpatterns` list routes URLs to views. For more information please see:
+
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+
+Examples:
+
+Function views
+
+    1. Add an import:  from my_app import views
+
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+
+Class-based views
+
+    1. Add an import:  from other_app.views import Home
+
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+
+Including another URLconf
+
+    1. Import the include() function: from django.urls import include, path
+
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+"""
+
+from django.contrib import admin
+def jls_extract_def(django, urls):
+    from django.urls import path, include
+    
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('tech/', include('tech.urls')),
+    ]
+    return urlpatterns
+
+
+urlpatterns = jls_extract_def(django, urls)
+
