1b4650fc38f941a4130647d1db5c93612fb84d9f
Minnie Yang <mimiwhite186@gmail.com>
2021-05-11 22:57:21 -0700

    resources view commit

--- tech/views.py
+++ tech/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .models import Meeting, MeetingMinutes, Resource, Event
 # Create your views here.
 def index(request):
     return render(request, 'tech/index.html')
+
+def resources(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'tech/resources.html', {'resource_list': resource_list})
